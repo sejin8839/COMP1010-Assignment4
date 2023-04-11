09df63cc45777a9c071daa1ef9f5f7b0ee272c2c
sejin8839 <sejinon01@gmail.com>
2023-04-11 23:12:09 +0900

    Create A4_shapes.py

---
+++ A4/A4_shapes.py
@@ -0,0 +1,20 @@
+
+# COMP1010 Assignment 4 problem1 starter  Code
+# Do not change anything in this file.
+
+# Do not change this code. Skip down to the area provided for your code.
+def draw_up_triangle():
+    print('   /\\') # \ is a special character, so to make an actual \ we need \\
+    print('  /  \\')
+    print(' /    \\')
+
+def draw_down_triangle():
+    print(' \\    /')
+    print('  \\  /')
+    print('   \\/')
+
+def draw_box():
+    print('+------+')
+    print('|      |')
+    print('|      |')
+    print('+------+')
